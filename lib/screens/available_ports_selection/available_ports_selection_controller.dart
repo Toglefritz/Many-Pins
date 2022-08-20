fbf1c07cae1838534dff4f92f6b5c1e815bbbb06
Toglefritz <contact@hatfieldscott.com>
2022-08-20 07:01:45 -0500

    Fixed duplication in serial ports list on refresh :bug:

--- lib/screens/available_ports_selection/available_ports_selection_controller.dart
+++ lib/screens/available_ports_selection/available_ports_selection_controller.dart
@@ -6,7 +6,7 @@ import 'package:flutter_libserialport/flutter_libserialport.dart';
 /// Controller for [AvailablePortsSelectionRoute].
 class AvailablePortsSelectionController
     extends State<AvailablePortsSelectionRoute> {
-  /// A list of [SerialPort]s available from the system.
+  // A list of [SerialPort]s available from the system.
   List<SerialPort> availablePorts = [];
 
   @override
@@ -18,12 +18,15 @@ class AvailablePortsSelectionController
   /// Get a list of available serial ports from the host system.
   void initPorts() {
     setState(() {
+      // Clear the list of serial ports before building it back up
+      availablePorts.clear();
+
+      // Get a list of serial ports by address
       List<String> availablePortAddresses = SerialPort.availablePorts;
 
+      // Build a list of SerialPort objects from the list of addresses
       for (String address in availablePortAddresses) {
-        if (!availablePorts.contains(SerialPort(address))) {
-          availablePorts.add(SerialPort(address));
-        }
+        availablePorts.add(SerialPort(address));
       }
     });
   }
